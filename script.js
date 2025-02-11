70b0ceb9edf0409e28ab9930ee772794f2f0ac18
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-11 10:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+  const {minHeap}= require('heap-js')
+	const pq= new minHeap();
+  let cost =0;
+
+for(let i =0;i<arr.length;i++){
+	pq.push(arr[i]);
+}
+	while(pq.length>1{
+		let top1 = pq.peek();
+		pq.pop();
+		 
+			let top2 = pq.peek();
+			pq.pop();
+         let val = top1+top2;
+		cost+=val;
+		pq.push(val);
+		
+		
+	}
+	return cost+pq.top();	
 }
 
 module.exports=mincost;
